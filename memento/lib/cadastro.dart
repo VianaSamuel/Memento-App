b96b68757dea7dea8dacdc61dbeb6d6140ea35b6
marcelogam <106875860+marcelogam@users.noreply.github.com>
2023-09-20 21:47:33 -0300

    Tela de login e Tela de cadastro

---
+++ memento/lib/cadastro.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class cadastro extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CadastroPage(),
+    );
+  }
+}
+
+class CadastroPage extends StatefulWidget {
+  @override
+  _CadastroPageState createState() => _CadastroPageState();
+}
+
+class _CadastroPageState extends State<CadastroPage> {
+  TextEditingController _nomeController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _senhaController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _nomeController,
+                decoration: InputDecoration(
+                  labelText: 'Nome',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _senhaController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Implemente aqui a lógica de cadastro
+                  String nome = _nomeController.text;
+                  String email = _emailController.text;
+                  String senha = _senhaController.text;
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginApp()),
+                  );
+                  // Aqui você pode salvar os dados do novo usuário ou realizar qualquer outra ação necessária.
+                  // Por exemplo, você pode exibir uma mensagem de sucesso ou redirecionar o usuário para a tela de login.
+                },
+                child: Text('Cadastrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ memento/lib/principal.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class principal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CadastroPage(),
+      theme:
+          ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 213, 21, 190))),
+    );
+  }
+}
+
+class CadastroPage extends StatefulWidget {
+  @override
+  _CadastroPageState createState() => _CadastroPageState();
+}
+
+class _CadastroPageState extends State<CadastroPage> {
+  TextEditingController _nomeController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _senhaController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _nomeController,
+                decoration: InputDecoration(
+                  labelText: 'Nome',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _senhaController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Implemente aqui a lógica de cadastro
+                  String nome = _nomeController.text;
+                  String email = _emailController.text;
+                  String senha = _senhaController.text;
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginApp()),
+                  );
+                  // Aqui você pode salvar os dados do novo usuário ou realizar qualquer outra ação necessária.
+                  // Por exemplo, você pode exibir uma mensagem de sucesso ou redirecionar o usuário para a tela de login.
+                },
+                child: Text('Cadastrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
