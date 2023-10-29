e6d345373f0a836f9cd3a831e82183eb1499abdd
VianaSamuel <samuluizzz@hotmail.com>
2023-10-29 13:13:00 -0300

    adição do gitignore

--- memento/android/app/src/main/kotlin/com/example/memento/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.memento
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- memento/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:memento/app_widget.dart';
-
-void main() {
-  runApp(AppWidget());
-}
